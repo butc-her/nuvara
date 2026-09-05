13a0fb2cd5ee964b0857bc3214348cdd08e36afa
Daniel Samson Sompien <theafricanchild19@gmail.com>
2026-09-05 14:58:44 +0100

    Update Navbar.tsx

--- nuvara/src/components/navbar/Navbar.tsx
+++ nuvara/src/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Link from "next/link";
 
-export default function Navbar() {
+export function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
